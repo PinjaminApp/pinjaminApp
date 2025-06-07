3c0422c35db89919449ca4c7e53def79f023b18f
Anja Bunga Aditya <2210511161@mahasiswa.upnvj.ac.id>
2025-06-07 09:39:06 +0700

    login

---
+++ app/src/main/java/com/example/pinjamin/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.pinjamin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        findViewById<TextView>(R.id.tvSignUp).setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
